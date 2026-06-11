e3d96e7820946e7f03f593269e2336c5914fd0d3
unknown <lxethanchen@gmail.com>
2026-06-12 03:20:36 +0800

    camera

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   flutter_webrtc
-  livekit_client
   url_launcher_linux
 )
 
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,10 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  connectivity_plus
   firebase_core
   flutter_webrtc
-  livekit_client
   permission_handler_windows
   url_launcher_windows
 )
